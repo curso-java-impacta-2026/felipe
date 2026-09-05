4fff13d4c13b2f6eea1a627713ba280fd4b3d9b0
FelipeDala0902 <felipe.dala.rodrigues@gmail.com>
2026-09-05 12:14:19 -0300

    Exemplo de interface padrão e defaul

--- exemplos/src/br/com/impacta/cap10/Eletrodomestico.java
+++ exemplos/src/br/com/impacta/cap10/Eletrodomestico.java
@@ -2,6 +2,28 @@ package br.com.impacta.cap10;
 
 public interface Eletrodomestico {
 
+	public final int VOLTAGEM = 110;
+	
     void ligar();
     void desligar();
+    
+//    
+    default void acionarTimer(int minutos) {
+
+        /* Aguarda a quantidade de minutos informada. */
+        try {
+        	Thread.sleep(minutos * 600);
+        	} catch (Exception e) {
+        System.out.println("Deu erro na theread!!!");
+        	}
+        /* Executa o método desligar conforme implementado na classe. */
+        desligar();
+      
+    }
+
+//    static void ligarTudo(Eletrodomestico... eletroArray) {
+//        for (Eletrodomestico eletro : eletroArray) {
+//            eletro.ligar();
+//        }
+//    }
 }
--- exemplos/src/br/com/impacta/cap10/Executando.java
+++ exemplos/src/br/com/impacta/cap10/Executando.java
@@ -4,15 +4,33 @@ public class Executando {
 
     public static void main(String[] args) {
 
-        Eletrodomestico aparelho;
+//       Eletrodomestico aparelho;
+//
+//        aparelho = new Geladeira();
+//        aparelho.ligar();
+//
+//        aparelho = new Televisao();
+//        aparelho.ligar();
+//
+//        aparelho = new Microondas();
+//        aparelho.ligar();
+//    	
+    	 Eletrodomestico tv = new Televisao();
 
-        aparelho = new Geladeira();
-        aparelho.ligar();
+         tv = new Televisao();
+         tv.ligar();
+         tv.acionarTimer(6);
+ //       Eletrodomestico a, b, c, d, e, f, g;
+//
+//        a = new Geladeira();
+//        b = new Televisao();
+//        c = new Televisao();
+//        d = new Geladeira();
+//        e = new Microondas();
+//        f = new Televisao();
+//        g = new Microondas();
+//
+//        Eletrodomestico.ligarTudo(a, b, c, d, e, f, g);
 
-        aparelho = new Televisao();
-        aparelho.ligar();
-
-        aparelho = new Microondas();
-        aparelho.ligar();
     }
 }
